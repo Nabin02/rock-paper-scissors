18fedbf79810e873130fc8b768389fcf8c53138d
Nabin Adhikari <nabinadhikari20@yahoo.com>
2022-04-27 01:02:40 -0400

    Set up single play of Rock Paper Scissor.

--- style.js
+++ style.js
@@ -1,10 +1,35 @@
 function computerPlay() {
-    let randomNumber = Math.floor(Math.random()*3) + 1;
-    if (randomNumber === 1) {
+    let randomNumber = Math.floor(Math.random()*2);
+    if (randomNumber === 0) {
         return ("Rock")
-    } else if  (randomNumber === 2) {
+    } else if  (randomNumber === 1) {
         return ("Paper")
-    } else if (randomNumber === 3) {
+    } else if (randomNumber === 2) {
         return ("Scissors")
     }
+}
+
+function roundPlay(playerSelection, computerSelection) {
+    const yourChoice = playerSelection.toLowerCase();
+    const computerPlay = computerSelection.toLowerCase();
+
+if (yourChoice === "rock" && computerPlay === "rock") {
+    return "Thats a Match, no one wins this round";
+} else if (yourChoice === "paper" && computerPlay === "paper") {
+    return "Thats a Match, no one wins this round";
+} else if (yourChoice === "scissors" && computerPlay === "scissors") {
+    return "Thats a Match, no one wins this round";
+} else if (yourChoice === "rock" && computerPlay === "scissors") {
+    return "You Win! Rock beats Scissors";
+} else if (yourChoice === "paper" && computerPlay === "rock") {
+    return "You Win! Paper beats Rock";
+} else if (yourChoice === "scissors" && computerPlay === "rock") {
+    return "You Loose! Rock beats Scissors";
+} else if (yourChoice === "rock" && computerPlay === "paper") {
+    return "You Loose! Paper Beats Rock";
+} else if (yourChoice === "paper" && computerPlay === "scissors") {
+    return "You Loose! Scissors beats Paper";
+} else if (yourChoice === "scissors" && computerPlay === "paper") {
+    return "You Win! Scissors beats Paper";
+}
 }
