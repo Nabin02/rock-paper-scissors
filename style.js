581a01648260d0c28ec7d90b8bfdc1675f2ec2e6
Nabin Adhikari <nabinadhikari20@yahoo.com>
2022-04-27 01:04:04 -0400

    Add game() function  to analyze game play.

--- style.js
+++ style.js
@@ -33,3 +33,93 @@ if (yourChoice === "rock" && computerPlay === "rock") {
     return "You Win! Scissors beats Paper";
 }
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let drawScore = 0;
+
+let answer = parseInt(prompt("Please enter the number of rounds you would like to play: "));
+
+for (let i = 0; i <= answer; i++) {
+const playerSelection = prompt("Choose one: Rock, Paper, or Scissors?")
+
+    if (
+        roundPlay(playerSelection, computerPlay()) ===
+        "Thats a Match, no one wins this round"
+    ) {
+        drawScore += 1;
+        console.log("Thats a Match, no one wins this round");
+    }
+    
+    else if (
+        roundPlay(playerSelection, computerPlay()) ===
+        "You Win! Rock beats Scissors"
+    ) {
+        playerScore += 1;
+        console.log("You Win! Rock beats Scissors");
+    }
+    
+    else if (
+        roundPlay(playerSelection, computerPlay()) ===
+        "You Win! Rock beats Scissors"
+    ) {
+        playerScore += 1;
+        console.log("You Win! Rock beats Scissors");
+    }
+    
+    else if (
+        roundPlay(playerSelection, computerPlay()) ===    
+        "You Win! Paper beats Rock"
+    ) {
+        playerScore += 1;
+        console.log("You Win! Paper beats Rock");
+    }
+    
+    else if (
+        roundPlay(playerSelection, computerPlay()) ===    
+        "You Loose! Rock beats Scissors"
+    ) {
+        computerScore += 1;
+        console.log("You Loose! Rock beats Scissors");
+    }
+    
+    else if (
+        roundPlay(playerSelection, computerPlay()) ===    
+        "You Loose! Paper Beats Rock"
+    ) {
+        computerScore += 1;
+        console.log("You Loose! Paper Beats Rock");
+    }
+    
+    else if (
+        roundPlay(playerSelection, computerPlay()) ===  
+        "You Loose! Scissors beats Paper"
+    ) {
+        computerScore += 1;
+        console.log("You Loose! Paper Beats Rock");
+    }
+    
+    else if (
+        roundPlay(playerSelection, computerPlay()) ===  
+        "You Win! Scissors beats Paper"
+    ) {
+        playerScore += 1;
+        console.log("You Win! Scissors beats Paper");
+    }
+    console.log("Your Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+    console.log("Number of Draw Games: " + drawScore);
+    console.log("Number of Games Played: " +i);
+}
+
+if (i = 5 && playerScore > computerScore) {
+    console.log("Congrats! You have beat a Computer. You are smarter than a Computer");
+} else if (i = 5 && playerScore < computerScore) {
+    console.log("True as old tales, you can not beat a computer. Ever!");
+} else if (i = 5 && playerScore === computerScore) {
+    console.log("Try Better Next Time! Only a Draw because the computer lagged a little LOL.");
+}
+}
+
+game();
